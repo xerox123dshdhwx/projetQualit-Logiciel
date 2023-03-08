cec830362babdaf206b1376bea70d7c6f4be67c7
MaelRibes <90307031+MaelRibes@users.noreply.github.com>
2023-03-08 11:23:52 +0100

    DateTimerTest done

--- src/test/java/timer/DateTimerTest.java
+++ src/test/java/timer/DateTimerTest.java
@@ -3,13 +3,17 @@ package timer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 import java.util.Vector;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DateTimerTest {
+
     Vector<Integer> lapsTimes;
     DateTimer dt;
+
     @BeforeEach
     void setUp() {
         lapsTimes = new Vector<>();
@@ -17,13 +21,22 @@ class DateTimerTest {
         lapsTimes.add(1);
         dt = new DateTimer(lapsTimes);
     }
+
+
     @Test
     void hasNext() {
+        assertTrue(dt.hasNext());
+        dt.next();
+        dt.next();
+        assertFalse(dt.hasNext());
     }
 
     @Test
     void next() {
         assertEquals(dt.next(),5);
         assertEquals(dt.next(),1);
+        NoSuchElementException thrown = assertThrows(NoSuchElementException.class, () -> {
+            dt.next();
+        });
     }
 }
